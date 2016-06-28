ad6a7678e0d8e1c70b1932e8d32595dadef02336
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-06-28 15:41:23 +0300

    Quiet upload.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 # s-post fails because it does not send the file as multipart as required by Fuseki.
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/skos.rdf" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/juho-skos.ttl" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/jupo-skos.ttl" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/liito-skos.ttl" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/ptvl-skos.ttl" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tero-skos.ttl" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tsr-skos.ttl" http://localhost:3030/all/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/yso-skos.ttl" http://localhost:3030/all/upload
+PORT=`netstat -lp | perl -0777 -ne 'print $1 if /\:(\d+)/'`
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/skos.rdf" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/juho-skos.ttl" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/jupo-skos.ttl" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/liito-skos.ttl" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/ptvl-skos.ttl" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tero-skos.ttl" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tsr-skos.ttl" http://localhost:$PORT/all/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/yso-skos.ttl" http://localhost:$PORT/all/upload
 
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/skos.rdf" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/juho-skos.ttl" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/jupo-skos.ttl" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/liito-skos.ttl" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/ptvl-skos.ttl" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tero-skos.ttl" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tsr-skos.ttl" http://localhost:3030/all_no_inference/upload
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/yso-skos.ttl" http://localhost:3030/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/skos.rdf" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/juho-skos.ttl" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/jupo-skos.ttl" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/liito-skos.ttl" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/ptvl-skos.ttl" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tero-skos.ttl" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/tsr-skos.ttl" http://localhost:$PORT/all_no_inference/upload
+curl -isS -X POST -H "Content-Type: multipart/form-data" -F "data=@ontologies/yso-skos.ttl" http://localhost:$PORT/all_no_inference/upload
